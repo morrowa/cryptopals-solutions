a6b9abc248021aa1b5fbbc170cd8ddb1e79f5cfa
Andrew Morrow <andrew.d.morrow@icloud.com>
2021-03-16 01:14:10 -0600

    Better version of set 1 challenge 3
    Does not consider strings with unprintable characters

--- src/bin/s1c3.rs
+++ src/bin/s1c3.rs
@@ -9,20 +9,18 @@ fn main() {
     let in_bytes = hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
     let mut results: Vec<(u8, f64, String)> = Vec::new();
     for i in 0..=255 {
-        let str_maybe = String::from_utf8(xor(&in_bytes, i));
-        if str_maybe.is_err() {
-            continue;
-        }
-        let decoded_string = str_maybe.unwrap();
-        if !decoded_string.is_ascii() {
-            continue;
-        }
-        let score = score_string(&decoded_string);
-        results.push((i, score, decoded_string));
+        String::from_utf8(xor(&in_bytes, i))
+            .into_iter()
+            .for_each(|s| {
+                match score_string(&s) {
+                    Ok(score) => results.push((i, score, s)),
+                    Err(_) => (),
+                };
+            });
     }
     results.sort_unstable_by(|x, y| x.1.partial_cmp(&y.1).unwrap().reverse());
     println!("Results in order:");
-    for (key, score, text) in &results[0..20] {
-        println!("{:#x} ({:.3}) {}", key, score, text.escape_default());
+    for (key, score, text) in results {
+        println!("{:#x} ({:.3}) {}", key, score, text);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,37 @@
+use std::error::Error;
+use std::fmt;
+
 use strsim::normalized_levenshtein;
+
+#[derive(Debug, Clone)]
+pub enum StringScoreError {
+    IllegalChars,
+}
+
+impl fmt::Display for StringScoreError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "string contains non-ASCII or unprintable characters")
+    }
+}
+
+impl Error for StringScoreError {}
 
 const SORTED_LETTERS: &'static str = "etaoinsrhdlucmfywgpbvkxqjz";
 
-pub fn score_string(text: &str) -> f64 {
+/// Scores the likelihood that a string is English plaintext by computing letter frequency. Only
+/// allows printable ASCII characters (i.e. 32 through 126 inclusive).
+pub fn score_string(text: &str) -> Result<f64, StringScoreError> {
+    if !text.is_ascii() { return Err(StringScoreError::IllegalChars); }
     let lower = text.to_ascii_lowercase();
     let mut counts: [u16; 256] = [0; 256];
     for b in lower.bytes() {
+        if b < 32 || b > 126 {
+            return Err(StringScoreError::IllegalChars);
+        }
         counts[b as usize] += 1;
     }
     let mut printable_by_freq: Vec<(u16, char)> = counts[32..127].iter().cloned().enumerate().filter(|(_, x)| *x > 0).map(|(i, x)| (x, (32 + i as u8) as char)).collect();
     printable_by_freq.sort_by_key(|x| x.0);
     let ordered_letters: String = printable_by_freq.iter().rev().map(|(_, x)| x ).collect();
-    normalized_levenshtein(&ordered_letters, SORTED_LETTERS)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn perfect_score() {
-        let test_str = "The quick red fox jumps over the lazy brown dog.";
-        println!("score: {}", score_string(test_str));
-    }
+    Ok(normalized_levenshtein(&ordered_letters, SORTED_LETTERS))
 }
