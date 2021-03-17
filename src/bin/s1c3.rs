0b2500c09a3508dac123ec1f9cf37899cb72f722
Andrew Morrow <andrew.d.morrow@icloud.com>
2021-03-16 18:47:50 -0600

    Set 1 challenge 4 complete
    Back to the old method for scoring possible plaintext strings, because the new one did not work well with this string

--- src/bin/s1c3.rs
+++ src/bin/s1c3.rs
@@ -1,27 +1,12 @@
-use cryptopals::score_string;
+use cryptopals::brute_force_single_byte_xor;
 use hex;
-
-fn xor(in_bytes: &[u8], key: u8) -> Vec<u8> {
-    in_bytes.iter().map(|x| x ^ key).collect()
-}
 
 fn main() {
     let in_bytes = hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
-    let mut results: Vec<(u8, f64, String)> = Vec::new();
-    for i in 0..=255 {
-        // if i == 0x58 {
-        //     println!("magic");
-        // }
-        String::from_utf8(xor(&in_bytes, i))
-            .ok()
-            .map(|s| {
-                score_string(&s)
-                    .map(|score| results.push((i, score, s)))
-            });
-    }
-    results.sort_unstable_by(|x, y| x.1.partial_cmp(&y.1).unwrap().reverse());
     println!("Results in order:");
-    for (key, score, text) in results {
-        println!("{:#x} ({:.4}) {}", key, score, text);
+    let mut results = brute_force_single_byte_xor(&in_bytes);
+    results.sort_unstable_by(|x, y| x.score.partial_cmp(&y.score).unwrap().reverse());
+    for r in results {
+        println!("{:#x} ({:.4}) {}", r.key, r.score, r.plaintext);
     }
 }
---
+++ src/bin/s1c4.rs
@@ -0,0 +1,32 @@
+extern crate hex;
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+use cryptopals::{brute_force_single_byte_xor, BruteForceResult};
+
+// We know that the final string will be ASCII. I checked all non-ASCII (but valid UTF-8) strings,
+// and they were all garbage.
+// So we only need to check ASCII strings.
+fn main() {
+    if env::args().len() != 2 {
+        panic!("wrong number of arguments");
+    }
+    let filename = env::args().next_back().unwrap();
+    let file = File::open(filename).expect("could not open file");
+    let reader = BufReader::new(file);
+    let mut results: Vec<(usize, BruteForceResult)> = reader.lines().enumerate()
+        .map(|(no, line)| {
+            brute_force_single_byte_xor(&hex::decode(line.unwrap()).unwrap())
+                .into_iter().map(move |r| (no, r))
+        })
+        .flatten()
+        .collect();
+    results.sort_unstable_by(|x, y| x.1.score.partial_cmp(&y.1.score).unwrap().reverse());
+
+    println!("Top result:");
+    let top = results.first().unwrap();
+    let r = &top.1;
+    println!("Line {} ({:#x}) ({:.4}) {}", top.0, r.key, r.score, r.plaintext.escape_default());
+}
+
