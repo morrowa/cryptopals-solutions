34d0875ec19a0fd7660cb2a828b3991ede189342
Andrew Morrow <andrew.d.morrow@icloud.com>
2021-04-20 22:54:22 -0600

    Tweaks to charfreq to count Unicode chars as their ASCII lookalikes

--- src/bin/charfreq.rs
+++ src/bin/charfreq.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::collections::HashMap;
 
 fn main() {
     if env::args().len() != 2 {
@@ -14,7 +14,18 @@ fn main() {
     file.read_to_string(&mut text).unwrap();
 
     let mut counts: HashMap<char, usize> = HashMap::new();
-    for chr in text.chars() {
+    for mut chr in text.chars().flat_map(char::to_lowercase) {
+        if chr == '\u{2013}' || chr == '\u{2014}' {
+            // map all dashes to simple ASCII dashes
+            chr = '-';
+        } else if chr == '\u{2018}' || chr == '\u{2019}' {
+            chr = '\'';
+        } else if chr == '\u{201c}' || chr == '\u{201d}' {
+            chr = '"';
+        } else if chr == '\r' || chr > 'z' {
+            // ignoring everything past ASCII
+            continue;
+        }
         let x = *counts.get(&chr).unwrap_or(&0);
         counts.insert(chr, x + 1);
     }
@@ -25,10 +36,10 @@ fn main() {
     for (chr, cnt) in counts {
         if chr.is_control() {
             print!("{}", chr.escape_default());
-        }
-        else {
+        } else {
             print!("'{}'", chr);
         }
-        println!("\t({}) {}", if chr.is_ascii() { "A" } else { "U" }, cnt);
+        // println!("\t({}) {}", if chr.is_ascii() { "A" } else { "U" }, cnt);
+        println!("\t({}) {}", chr.escape_unicode(), cnt);
     }
 }
